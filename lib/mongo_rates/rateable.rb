1bded640619764435e3fabb82898c64eee35af5b
Justin Ridgewell <justin@ridgewell.name>
2013-04-30 20:47:08 -0400

    Implement average_rating with map_reduce

--- lib/mongo_rates/rateable.rb
+++ lib/mongo_rates/rateable.rb
@@ -22,9 +22,17 @@ module MongoRates
         rateable_ratings = ratings
         return 0 if ratings.empty?
 
-        rateable_ratings.all.reduce(0.0) { |sum, rating|
-          sum += rating.value
-        } / rateable_ratings.size
+        map = %Q(function() {
+          if (this.rateable_type == '#{self.class.to_s}' && this.rateable_id == #{id}) {
+            emit(this.rateable_id, this.value);
+          }
+        })
+        reduce = %Q(function(id, values) {
+          return Array.sum(values) / values.length;
+        })
+        output_collection = "mongo_rates.models.ratings.#{self.class.to_s.downcase}#{id}"
+
+        rateable_ratings.collection.map_reduce(map, reduce, :out => output_collection).find()
       end
 
       def rated?
